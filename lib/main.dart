683984ae4a5fd72f4122c6b104996ade34d9ec17
melmv <bmoisesg@gmail.com>
2023-08-24 22:58:46 -0600

    update - bloqueo para rotar pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:encuentas/color.dart';
 import 'package:encuentas/screens/login.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart' show Firebase;
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +16,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
